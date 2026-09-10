b2530a4ea6f6d8c524a01ced0d562fd4c564e013
student <student@CLB5COMP29.Server2.edu.ph>
2026-09-10 09:53:22 +0800

    Add viteProjet01

---
+++ projects/viteProjet01/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <p>PHIGHTING!</p>
+        <h1>My Favorite Skin</h1>
+      </header>
+
+      <section className="showcase">
+        <div className="character">
+          <img src="/Render_Skin_Grieferspace.png" alt="My Favorite Skin" />
+        </div>
+
+        <div className="description">
+          <h2>Why I like this skin</h2>
+          <p>
+            I THINK THIS SKIN IS SO COOL BECAUSE IT HAS PLANT FEATURES IN IT,
+            PLANT FEATURES I'S A COOL CONCEPT
+            DON'T FORGET, THIS IS A COLAB SKIN FROM ANOTHER GAME!
+            AND THE CHARACTER CAN PUT POISON TO ENEMIES!!!
+            IT IS A SUPPORT CHARACTER, BUT THE BEST SUPPORT CHARACTER
+          </p>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
